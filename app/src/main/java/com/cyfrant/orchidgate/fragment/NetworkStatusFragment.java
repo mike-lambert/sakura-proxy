04a3c6dceef46206a38dbd751831c9b3c3847649
Mike Lambert <cyberspacelabs@gmail.com>
2019-02-09 19:38:57 +0300

    2.2.29 - status lines aligned

--- app/src/main/java/com/cyfrant/orchidgate/fragment/NetworkStatusFragment.java
+++ app/src/main/java/com/cyfrant/orchidgate/fragment/NetworkStatusFragment.java
@@ -32,6 +32,7 @@ public class NetworkStatusFragment extends Fragment {
 
     private TextView coreVersion;
     private TextView addresses;
+
     public NetworkStatusFragment() {
         // Required empty public constructor
     }
@@ -65,24 +66,57 @@ public class NetworkStatusFragment extends Fragment {
         String appVersionText = getString(R.string.status_version_app).replace("{}", getAppVersion());
         String coreVersionText = getString(R.string.status_version_core).replace("{}",
                 ApplicationProperties.getName() + "-" + ApplicationProperties.getVersion()
-                );
+        );
         String osVersionText = getString(R.string.status_version_os).replace("{}", Build.VERSION.RELEASE);
         String sdkLevelText = getString(R.string.status_level_sdk).replace("{}", Integer.toString(Build.VERSION.SDK_INT));
         String deviceNameText = getString(R.string.status_device).replace("{}",
                 Build.MANUFACTURER + " " + Build.MODEL
-                );
-        String info = appVersionText + "\r\n" +
-                coreVersionText + "\r\n" +
-                osVersionText + "\r\n" +
-                sdkLevelText + "\r\n" +
-                deviceNameText;
+        );
+
+        String info = alignAndMergeText(appVersionText,
+                coreVersionText,
+                osVersionText,
+                sdkLevelText,
+                deviceNameText);
         coreVersion.setText(info);
         addresses.setText(join(enumeratePublicAddresses()));
     }
 
+    private String alignAndMergeText(String... strings) {
+        StringBuffer result = new StringBuffer();
+        int delim = -1;
+        for (String string : strings) {
+            delim = Math.max(delim, string.indexOf(':'));
+        }
+
+        for (String string : strings) {
+            int curr = string.indexOf(':');
+            int pad = (delim - curr) + 1;
+            String prefix = padRight(string.substring(0, curr), pad);
+            String trail = string.substring(curr + 1);
+            result
+                    .append(prefix)
+                    .append(':')
+                    .append(' ')
+                    .append(trail)
+                    .append('\r')
+                    .append('\n');
+        }
+
+        return result.toString();
+    }
+
+    private String padRight(String text, int pad) {
+        String result = text;
+        for (int i = 0; i < pad; i++) {
+            result += " ";
+        }
+        return result;
+    }
+
     private String join(List<String> strings) {
         StringBuffer result = new StringBuffer();
-        for(String entry : strings){
+        for (String entry : strings) {
             result.append(entry).append("\r\n");
         }
         return result.toString();
@@ -106,11 +140,11 @@ public class NetworkStatusFragment extends Fragment {
         List<String> result = new ArrayList<>();
         try {
             Enumeration<NetworkInterface> eni = NetworkInterface.getNetworkInterfaces();
-            while(eni.hasMoreElements()) {
+            while (eni.hasMoreElements()) {
                 NetworkInterface ni = eni.nextElement();
                 Enumeration<InetAddress> nia = ni.getInetAddresses();
 
-                while(nia.hasMoreElements()) {
+                while (nia.hasMoreElements()) {
                     InetAddress address = nia.nextElement();
                     if (address.isLoopbackAddress() || address.isLinkLocalAddress() || address.isSiteLocalAddress()) {
                         continue;
@@ -126,12 +160,15 @@ public class NetworkStatusFragment extends Fragment {
         } catch (SocketException e) {
             e.printStackTrace();
         }
+        if (result.isEmpty()){
+            result.add("<" + getString(R.string.status_no_grips) + ">");
+        }
         return result;
     }
 
     private String canonicalizeV6Address(Inet6Address address) {
         String addr = address.getHostAddress();
-        if (addr.contains("%")){
+        if (addr.contains("%")) {
             addr = addr.substring(0, addr.indexOf('%'));
         }
         return addr;
@@ -139,7 +176,7 @@ public class NetworkStatusFragment extends Fragment {
 
     private String machex(byte[] address) {
         StringBuilder mac = new StringBuilder();
-        for(byte b : address) {
+        for (byte b : address) {
             mac.append(String.format("%02x", b)).append(":");
         }
         String ms = mac.toString();
