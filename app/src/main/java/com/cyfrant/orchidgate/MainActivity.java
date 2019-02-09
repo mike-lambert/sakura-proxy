a30686c15ab1fb6bde75a43dc4a97eec074134d5
Mike Lambert <cyberspacelabs@gmail.com>
2019-02-09 16:09:26 +0300

    2.2.24 - basically working fragments

--- app/src/main/java/com/cyfrant/orchidgate/MainActivity.java
+++ app/src/main/java/com/cyfrant/orchidgate/MainActivity.java
@@ -2,49 +2,25 @@ package com.cyfrant.orchidgate;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
-import android.net.ConnectivityManager;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.PowerManager;
-import android.provider.Settings;
-import android.view.View;
-import android.widget.Button;
-import android.widget.CompoundButton;
-import android.widget.ProgressBar;
-import android.widget.Switch;
-import android.widget.TextView;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.RelativeLayout;
 
 import com.cyfrant.orchidgate.application.ProxyApplication;
-import com.cyfrant.orchidgate.contract.Proxy;
-import com.cyfrant.orchidgate.contract.ProxyStatusCallback;
-import com.cyfrant.orchidgate.service.ProxyManager;
-import com.demo.ApplicationProperties;
+import com.cyfrant.orchidgate.fragment.StatusFragment;
 
-import java.text.DecimalFormat;
-
-public class MainActivity extends Activity implements ProxyStatusCallback {
-    private static final DecimalFormat secondFormat = new DecimalFormat("#0.0");
-    private static final String PACKAGE_TELEGRAM = "org.telegram.messenger";
-    private static final String PACKAGE_TELEGRAMX = "org.thunderdog.challegram";
-    private static final int COLOR_LIGHT_GREEN = Color.parseColor("#FFD4FFBF");
-    private static final int COLOR_LIGHT_YELLOW = Color.parseColor("#FFFFFDBF");
-    private static final int COLOR_LIGHT_RED = Color.parseColor("#FFFFC6BF");
-
-    private Switch enableSwitch;
-    private Button linkButton;
-    private ProgressBar bootProgress;
-    private TextView bootStatus;
-    private TextView heartbeatStatus;
-    private TextView coreVersion;
-    private int port;
+public class MainActivity extends Activity {
+    private enum Screen {
+        ProxyStatus,
+        NetworkStatus,
+        Settings
+    }
+    private RelativeLayout viewport;
+    private BottomNavigationView navigation;
+    private Screen screen;
 
     protected ProxyApplication getProxyApplication() {
         return (ProxyApplication) getApplication();
@@ -54,38 +30,23 @@ public class MainActivity extends Activity implements ProxyStatusCallback {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        coreVersion = findViewById(R.id.textCoreVersion);
-        coreVersion.setText(
-                "App version : " +
-                        (getPackageInfo(getPackageName()) != null ?
-                                getPackageInfo(getPackageName()).versionName :
-                                "<unknown>"
-                        ) +
-                        "\r\nRouter core : "
-                        + ApplicationProperties.getName()
-                        + "-" + ApplicationProperties.getVersion()
-                        + "\r\nOS          : " + Build.VERSION.RELEASE
-                        + "\r\nSDK         : " + Build.VERSION.SDK_INT
-                        + "\r\nDevice      : " + Build.MANUFACTURER + " " + Build.MODEL
-        );
-        enableSwitch = findViewById(R.id.switchEnableProxy);
-        linkButton = findViewById(R.id.buttonTelegramLink);
-        bootProgress = findViewById(R.id.progressBoot);
-        bootStatus = findViewById(R.id.statusBoot);
-        heartbeatStatus = findViewById(R.id.statusHeartbeat);
-        enableSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        viewport = findViewById(R.id.viewport);
+        navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    getProxyApplication().getProxyController().startProxyService();
-                    heartbeatStatus.setText("");
-                    heartbeatStatus.setBackgroundColor(Color.WHITE);
-                } else {
-                    getProxyApplication().getProxyController().stopProxyService();
-                    linkButton.setVisibility(View.GONE);
-                    heartbeatStatus.setText("");
-                    heartbeatStatus.setBackgroundColor(Color.WHITE);
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.menu_status:
+                        screen = Screen.ProxyStatus;
+                        displayProxyStatusFragment();
+                        return true;
+
+                    case R.id.menu_settings:
+                        startSettingsActivity();
+                        return true;
+
                 }
+                return false;
             }
         });
     }
@@ -93,250 +54,55 @@ public class MainActivity extends Activity implements ProxyStatusCallback {
     @Override
     protected void onResume() {
         super.onResume();
-        getProxyApplication().addProxyObserver(this);
-        syncState(getProxyApplication().isActive(),
-                getProxyApplication().getProxy() != null &&
-                        getProxyApplication().getProxy().isStartPending()
-        );
-        checkWhitelisting();
+        restoreView();
+    }
+
+    private void restoreView() {
+        if (screen == null){
+            screen = Screen.ProxyStatus;
+        }
+        onPrepareOptionsMenu(navigation.getMenu());
+        dispatchFragment();
+    }
+
+    private void dispatchFragment() {
+        switch (screen){
+            case ProxyStatus:
+                displayProxyStatusFragment();
+                break;
+            default:
+                break;
+        }
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        getProxyApplication().removeProxyObserver(this);
-    }
-
-    // ProxyStatusCallback
-
-    @Override
-    public void onStartup(final int percentage, final String message) {
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                bootStatus.setVisibility(View.VISIBLE);
-                bootProgress.setVisibility(View.VISIBLE);
-                heartbeatStatus.setVisibility(View.VISIBLE);
-                linkButton.setVisibility(View.GONE);
-
-                bootStatus.setText(percentage + "% : " + message);
-            }
-        });
     }
 
     @Override
-    public void onStarted(final int port) {
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                MainActivity.this.port = port;
-                bootStatus.setVisibility(View.GONE);
-                bootProgress.setVisibility(View.GONE);
-                heartbeatStatus.setVisibility(View.VISIBLE);
-                linkButton.setVisibility(View.VISIBLE);
-                enableSwitch.setChecked(true);
-
-                heartbeatStatus.setText("Listening on 127.0.0.1:" + port);
-                syncLinkButton();
-            }
-        });
-    }
-
-    @Override
-    public void onStopped(final String cause) {
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                bootStatus.setVisibility(View.GONE);
-                bootProgress.setVisibility(View.GONE);
-                linkButton.setVisibility(View.GONE);
-                enableSwitch.setChecked(false);
-
-                heartbeatStatus.setText(cause);
-            }
-        });
-    }
-
-    @Override
-    public void onHeartbeat(final double delayUp, final double delayDown, final String exitAddress) {
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                heartbeatStatus.setText(exitAddress
-                        + ": ⬆" + secondFormat.format(delayUp)
-                        + " sec  ⬇" + secondFormat.format(delayDown) + " sec");
-                setHeartbeatBackground(delayDown + delayUp);
-            }
-        });
-    }
-
-    private void syncState(boolean running, boolean starting) {
-        enableSwitch.setChecked(running);
-        linkButton.setVisibility(running && !starting ? View.VISIBLE : View.GONE);
-        heartbeatStatus.setVisibility(View.VISIBLE);
-        bootStatus.setVisibility(starting ? View.VISIBLE : View.GONE);
-        bootProgress.setVisibility(starting ? View.VISIBLE : View.GONE);
-        port = (getProxyApplication().getProxyController().getProxy() != null ? getProxyApplication().getProxyController().getProxy().getProxyPort() : 0);
-        syncLinkButton();
-    }
-
-    private void setHeartbeatBackground(double rtt) {
-        if (rtt < 3) {
-            heartbeatStatus.setBackgroundColor(COLOR_LIGHT_GREEN);
-            return;
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        boolean result = super.onPrepareOptionsMenu(menu);
+        switch (screen){
+            case ProxyStatus:
+                menu.findItem(R.id.menu_status).setChecked(true);
+                break;
+            default:
+                break;
         }
-
-        if (rtt < 8) {
-            heartbeatStatus.setBackgroundColor(COLOR_LIGHT_YELLOW);
-            return;
-        }
-
-        heartbeatStatus.setBackgroundColor(COLOR_LIGHT_RED);
+        return result;
     }
 
-    private void syncLinkButton() {
-        Drawable telegram = getAppInfo(PACKAGE_TELEGRAM);
-        Drawable telegramx = getAppInfo(PACKAGE_TELEGRAMX);
-        linkButton.setCompoundDrawables(getLinkIcon(), null, null, null);
-
-        if (telegram == null && telegramx == null) {
-            linkButton.setText(R.string.label_button_install);
-            linkButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    launchPlayMarket(PACKAGE_TELEGRAM);
-                }
-            });
-            return;
-        }
-
-        linkButton.setText(R.string.label_button_link);
-        linkButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Proxy proxy = getProxyApplication().getProxy();
-                if (proxy == null) {
-                    return;
-                }
-                launchProxyScheme(ProxyManager.makeTelegramLink(proxy.getProxyPort()));
-            }
-        });
+    private void startSettingsActivity() {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
     }
 
-    private Drawable getLinkIcon() {
-        Drawable defaultIcon = scaleDrawable(getDrawable(R.drawable.google_play), 48);
-        defaultIcon.setBounds(0, 0, 48, 48);
-
-        Drawable tgx = getApplicationIcon(PACKAGE_TELEGRAMX);
-        Drawable tg = getApplicationIcon(PACKAGE_TELEGRAM);
-        if (tgx == null && tg == null) {
-            return defaultIcon;
-        }
-
-        if (tgx != null) {
-            Drawable scaledTgx = scaleDrawable(tgx, 48);
-            scaledTgx.setBounds(0, 0, 48, 48);
-            return scaledTgx;
-        }
-
-        if (tg != null) {
-            Drawable scaledTg = scaleDrawable(tg, 48);
-            scaledTg.setBounds(0, 0, 48, 48);
-            return scaledTg;
-        }
-        return defaultIcon;
-    }
-
-    private Drawable getAppInfo(String packageName) {
-        try {
-            return getPackageManager().getApplicationIcon(packageName);
-        } catch (PackageManager.NameNotFoundException e) {
-            return null;
-        }
-    }
-
-    private Drawable getApplicationIcon(String packageName) {
-        try {
-            return getPackageManager().getApplicationIcon(packageName);
-        } catch (PackageManager.NameNotFoundException e) {
-            return null;
-        }
-    }
-
-    public Drawable scaleDrawable(Drawable drawable, int dp) {
-        Bitmap src = drawableToBitmap(drawable);
-        BitmapDrawable d = new BitmapDrawable(Bitmap.createScaledBitmap(src, dp, dp, true));
-        return d;
-    }
-
-    public static Bitmap drawableToBitmap(Drawable drawable) {
-        if (drawable instanceof BitmapDrawable) {
-            return ((BitmapDrawable) drawable).getBitmap();
-        }
-
-        int width = drawable.getIntrinsicWidth();
-        width = width > 0 ? width : 1;
-        int height = drawable.getIntrinsicHeight();
-        height = height > 0 ? height : 1;
-
-        Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
-        Canvas canvas = new Canvas(bitmap);
-        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
-        drawable.draw(canvas);
-
-        return bitmap;
-    }
-
-    private void launchPlayMarket(String packageName) {
-        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + packageName)));
-    }
-
-    private void launchProxyScheme(String link) {
-        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(link)));
-    }
-
-    @Override
-    public void onTorBootstrapFailed(final String message) {
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                heartbeatStatus.setText(message);
-                heartbeatStatus.setBackgroundColor(COLOR_LIGHT_RED);
-            }
-        });
-    }
-
-    private PackageInfo getPackageInfo(String packageName) {
-        try {
-            return getPackageManager().getPackageInfo(packageName, 0);
-        } catch (PackageManager.NameNotFoundException e) {
-            return null;
-        }
-    }
-
-    private void checkWhitelisting() {
-        final String packageName = getPackageName();
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            Intent intent = new Intent();
-            PowerManager pm = (PowerManager) getSystemService(POWER_SERVICE);
-            if (!pm.isIgnoringBatteryOptimizations(packageName)) {
-                intent.setAction(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS);
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                intent.setData(Uri.parse("package:" + packageName));
-                startActivity(intent);
-            }
-        }
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            Intent intent = new Intent();
-            ConnectivityManager connMgr = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
-            if (connMgr.getRestrictBackgroundStatus() == ConnectivityManager.RESTRICT_BACKGROUND_STATUS_ENABLED) {
-                intent.setAction(Settings.ACTION_IGNORE_BACKGROUND_DATA_RESTRICTIONS_SETTINGS);
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                intent.setData(Uri.parse("package:" + packageName));
-                startActivity(intent);
-            }
-
-        }
+    private void displayProxyStatusFragment() {
+        getFragmentManager()
+                .beginTransaction()
+                .replace(R.id.viewport, StatusFragment.newInstance())
+                .addToBackStack("proxy")
+                .commit();
     }
 }
